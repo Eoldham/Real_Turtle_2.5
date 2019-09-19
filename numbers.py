b62f5201b982415f06a44eaa2268ad3fbee17559
Allison Z <alzig@udel.edu>
2019-09-19 12:43:10 -0400

    Blah

---
+++ numbers.py
@@ -0,0 +1,180 @@
+import turtle
+
+def turtle_speed():
+    turtle.speed(50)
+
+
+def space():
+    turtle.penup()
+    turtle.setheading(0)
+    turtle.right(90)
+    turtle.forward(25)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.pendown()
+
+
+def number_zero():
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(180)
+    turtle.forward(25)
+
+
+def number_one():
+    turtle.penup()
+    turtle.forward(15)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(35)
+
+
+def number_two():
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(35)
+    turtle.pendown()
+
+
+def number_three():
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(5)
+    turtle.pendown()
+
+
+def number_four():
+    turtle.penup()
+    turtle.forward(30)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(180)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(35)
+
+
+def number_five():
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(35)
+    turtle.pendown()
+
+
+def number_six():
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(180)
+    turtle.forward(25)
+
+
+def number_seven():
+    turtle.penup()
+    turtle.forward(30)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(35)
+    turtle.pendown()
+
+
+def number_eight():
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(180)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(5)
+
+
+def number_nine():
+    turtle.penup()
+    turtle.forward(30)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(180)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(5)
+    turtle.pendown()
