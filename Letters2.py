73c7526783953a382699204a968006fdba53612a
Haritima23 <haritima@udel.edu>
2019-09-19 13:00:06 -0400

    Next 10 letters.

---
+++ Letters2.py
@@ -0,0 +1,146 @@
+import turtle
+
+
+def space():
+    turtle.penup()
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.pendown()
+
+
+def Letter_K():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.backward(50)
+    turtle.right(45)
+    turtle.forward(70)
+    turtle.backward(70)
+    turtle.right(90)
+    turtle.forward(70)
+
+
+def Letter_L():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.backward(100)
+    turtle.right(90)
+    turtle.forward(30)
+
+
+def Letter_M():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(135)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.right(135)
+    turtle.forward(100)
+    turtle.left(90)
+
+
+def Letter_N():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(135)
+    turtle.forward(140)
+    turtle.left(135)
+    turtle.forward(100)
+    turtle.backward(100)
+    turtle.right(90)
+
+
+def Letter_O():
+    turtle.circle(45)
+
+
+def Letter_P():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(40)
+    turtle.right(90)
+    turtle.forward(40)
+    turtle.right(90)
+    turtle.forward(40)
+    turtle.left(90)
+    turtle.forward(60)
+
+
+def Letter_Q():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.backward(25)
+    turtle.right(45)
+    turtle.forward(20)
+    turtle.backward(20)
+    turtle.left(185)
+    turtle.forward(50)
+    turtle.backward(50)
+
+
+def Letter_R():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.left(135)
+    turtle.forward(70)
+
+
+def Letter_S():
+    turtle.left(90)
+    turtle.circle(10, 180, 10)
+    turtle.left(45)
+    turtle.forward(30)
+    turtle.right(45)
+    turtle.forward(10)
+    turtle.right(45)
+    turtle.forward(10)
+    turtle.right(45)
+    turtle.forward(10)
+    turtle.right(45)
+    turtle.forward(10)
+
+
+def Letter_T():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.backward(40)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+
+
+Letter_L()
+space()
+Letter_M()
+space()
+Letter_N()
+space()
+Letter_O()
+space()
+Letter_P()
+space()
+Letter_Q()
+space()
+Letter_R()
+space()
+Letter_S()
+space()
+Letter_T()
+space()
+
